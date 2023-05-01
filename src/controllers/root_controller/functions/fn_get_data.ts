e73f6c82d84d1e3e617b263520dc7e17258cd0b5
Danendz <danilapayne@gmail.com>
2023-05-01 17:46:11 +0500

    some refactoring and add new controller for schema route

---
+++ src/controllers/root_controller/functions/fn_get_data.ts
@@ -0,0 +1,4 @@
+import { assetsData } from 'config/paths'
+import { getDirList } from 'shared/functions/fn_file_system'
+
+export default async () => await getDirList(assetsData())
---
+++ src/controllers/schema_controller/functions/fn_get_schema.ts
@@ -0,0 +1,16 @@
+import { assetsSchema } from 'config/paths'
+import path from 'path'
+import { getDirList, getFileByPath } from 'shared/functions/fn_file_system'
+import type { SchemaType } from '../types/schema'
+
+async function fn_get_schema(): Promise<string[]>
+async function fn_get_schema(name: string): Promise<SchemaType>
+async function fn_get_schema(name?: string): Promise<string[] | SchemaType> {
+  if (!name) {
+    return await getDirList(assetsSchema())
+  }
+  const schemaPath = path.join(assetsSchema(), name)
+  return await getFileByPath(schemaPath, 'schema.json')
+}
+
+export default fn_get_schema
